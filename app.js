e199d7f437fe0370b0889f0de227b24dae8cbc19
Patten <ejpatten@labs.neit.edu>
2018-01-18 14:21:57 -0500

    app.js update

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ var port = 3000;
 
 app.use(express.static('src'));
 
-app.get("/", function(req, res){
-	res.send("Wouldn't you like to be a pepper, too?");
-});
-
-app.get("/games", function(req, res){
-	res.send("Wouldn't you like to play games?");
-});
-
 //Listen on the port 
 app.listen(port, function(err){
 	console.log("Server is running on port " + port);
